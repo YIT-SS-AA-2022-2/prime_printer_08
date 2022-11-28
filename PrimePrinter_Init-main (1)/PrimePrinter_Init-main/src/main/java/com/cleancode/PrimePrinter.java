e9d1bf839282739ee9910d169fdbe2b092ddb464
jongddak <hfsmsj316@gmail.com>
2022-11-28 13:38:55 +0900

    변수명 변경

--- PrimePrinter_Init-main (1)/PrimePrinter_Init-main/src/main/java/com/cleancode/PrimePrinter.java
+++ PrimePrinter_Init-main (1)/PrimePrinter_Init-main/src/main/java/com/cleancode/PrimePrinter.java
@@ -3,30 +3,29 @@ package com.cleancode;
 public class PrimePrinter {
     public static void main(String[] args) {
         //start!!
-        final int M = 1000;
-        final int RR = 50;
-        final int CC = 4;
+        final int rowNum = 1000;
+        final int colNum = 50;
+        final int Num = 4;
         final int ORDMAX = 30;
-        int P[] = new int[M+1];
+        int P[] = new int[rowNum+1];
         int PAGENUMBER;
         int PAGEOFFSET;
         int ROWOFFSET;
         int C;
-        int J;
-        int K;
+        int J=1;
+        int K=1;
         boolean JPRIME;
-        int ORD;
-        int SQUARE;
+        int ORD = 2;
+        int SQUARE = 9;
         int N=0;
         int MULT[] = new int[ORDMAX+1];
 
-        J=1;
-        K=1;
-        P[1] = 2;
-        ORD = 2;
-        SQUARE = 9;
 
-        while (K < M) {
+        P[1] = 2;
+        // ??
+
+
+        while (K < rowNum) {
             do {
                 J += 2;
                 if( J == SQUARE) {
@@ -49,21 +48,21 @@ public class PrimePrinter {
         }
         PAGENUMBER = 1;
         PAGEOFFSET = 1;
-        while (PAGEOFFSET <= M) {
+        while (PAGEOFFSET <= rowNum) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(M));
+            System.out.print(Integer.toString(rowNum));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(PAGENUMBER));
             System.out.println("\n");
-            for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+RR-1; ROWOFFSET++) {
-                for (C = 0; C <= CC - 1; C++)
-                    if (ROWOFFSET + C * RR <= M)
-                        System.out.printf("%10d", P[ROWOFFSET + C * RR]);
+            for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+colNum-1; ROWOFFSET++) {
+                for (C = 0; C <= Num - 1; C++)
+                    if (ROWOFFSET + C * colNum <= rowNum)
+                        System.out.printf("%10d", P[ROWOFFSET + C * colNum]);
                 System.out.println();
             }
             System.out.println("\f");
             PAGENUMBER++;
-            PAGEOFFSET += RR*CC;
+            PAGEOFFSET += colNum*Num;
 
         }
     }
