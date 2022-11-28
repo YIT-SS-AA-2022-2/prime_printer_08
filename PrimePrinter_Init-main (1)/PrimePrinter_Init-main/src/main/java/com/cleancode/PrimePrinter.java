81dd19e6b80af02ceefb98fa2fded6053fb3fae2
DO <ehrbs2992@naver.com>
2022-11-28 21:11:12 +0900

    PrimePrinter 클래스에 책임이 많아서 RowColumnPagePrinter와 PrimeGenerator 클래스를 만들어서 분산시켰다  형식을 맞추고 변수명을 정립해서 코드가 길어졌지만 읽고 이해할 수 있다!

--- PrimePrinter_Init-main (1)/PrimePrinter_Init-main/src/main/java/com/cleancode/PrimePrinter.java
+++ PrimePrinter_Init-main (1)/PrimePrinter_Init-main/src/main/java/com/cleancode/PrimePrinter.java
@@ -1,62 +1,14 @@
-package com.cleancode;
-
 public class PrimePrinter {
     public static void main(String[] args) {
-        final int M = 1000;
-        final int RR = 50;
-        final int CC = 4;
-        final int ORDMAX = 30;
-        int P[] = new int[M+1];
-        int PAGENUMBER = 1;
-        int PAGEOFFSET = 1;
-        int ROWOFFSET;
-        int C;
-        int J = 1;
-        int K = 1;
-        boolean JPRIME;
-        int ORD = 2;
-        int SQUARE = 9;
-        int N=0;
-        int MULT[] = new int[ORDMAX+1];
+        final int NUMBER_OF_PRIMES = 1000;
+        int[] primes = PrimeGenerator.generate(NUMBER_OF_PRIMES);
 
-        P[1] = 2;
-
-        while (K < M) {
-            do {
-                J += 2;
-                if( J == SQUARE) {
-                    ORD++;
-                    SQUARE=P[ORD]*P[ORD];
-                    MULT[ORD-1]=J;
-                }
-                N=2;
-                JPRIME=true;
-                while (N < ORD && JPRIME) {
-                    while (MULT[N]<J)
-                        MULT[N] += P[N] + P[N];
-                    if (MULT[N] == J)
-                        JPRIME=false;
-                    N++;
-                }
-            } while (!JPRIME);
-            K++;
-            P[K]=J;
-        }
-        while (PAGEOFFSET <= M) {
-            System.out.print("The First ");
-            System.out.print(Integer.toString(M));
-            System.out.print(" Prime Numbers === Page ");
-            System.out.print(Integer.toString(PAGENUMBER));
-            System.out.println("\n");
-            for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+RR-1; ROWOFFSET++) {
-                for (C = 0; C <= CC - 1; C++)
-                    if (ROWOFFSET + C * RR <= M)
-                        System.out.printf("%10d", P[ROWOFFSET + C * RR]);
-                System.out.println();
-            }
-            System.out.println("\f");
-            PAGENUMBER++;
-            PAGEOFFSET += RR*CC;
-        }
+        final int ROWS_PER_PAGE = 50;
+        final int COLUMNS_PER_PAGE = 4;
+        RowColumnPagePrinter tablePrinter =
+                new RowColumnPagePrinter(ROWS_PER_PAGE,
+                        COLUMNS_PER_PAGE,
+                        "The First " + NUMBER_OF_PRIMES + " Prime Numbers");
+        tablePrinter.print(primes);
     }
 }
