7514c7ae324508a8b9c78ed2ade47728264ca0e4
jongddak <hfsmsj316@gmail.com>
2022-11-28 20:37:50 +0900

    변수명 변경(readme 참고) 및 메서드 추출

--- PrimePrinter_Init-main (1)/PrimePrinter_Init-main/src/main/java/com/cleancode/PrimePrinter.java
+++ PrimePrinter_Init-main (1)/PrimePrinter_Init-main/src/main/java/com/cleancode/PrimePrinter.java
@@ -3,67 +3,107 @@ package com.cleancode;
 public class PrimePrinter {
     public static void main(String[] args) {
         //start!!
-        final int rowNum = 1000;
-        final int colNum = 50;
-        final int Num = 4;
-        final int ORDMAX = 30;
-        int P[] = new int[rowNum+1];
+
+        int Primes[] = new int[getLinePageRowCol(1000, 1)];
         int PAGENUMBER;
         int PAGEOFFSET;
         int ROWOFFSET;
-        int C;
-        int J=1;
-        int K=1;
-        boolean JPRIME;
+        int column;
+        int canDidate=1;
+        int PrimeIndex=1;
+        boolean possiblyPrime;
         int ORD = 2;
         int SQUARE = 9;
         int N=0;
-        int MULT[] = new int[ORDMAX+1];
+        int multiples[] = new int[getLinePageRowCol(30, 1)];
 
 
-        P[1] = 2;
+        Primes[1] = 2;
         // ??
 
 
-        while (K < rowNum) {
+        while (PrimeIndex < 1000) {
             do {
-                J += 2;
-                if( J == SQUARE) {
+                canDidate += 2;
+                if( canDidate == SQUARE) {
                     ORD++;
-                    SQUARE=P[ORD]*P[ORD];
-                    MULT[ORD-1]=J;
+                    SQUARE= getColumn(Primes[ORD], Primes[ORD]);
+                    multiples[ORD-1]=canDidate;
                 }
                 N=2;
-                JPRIME=true;
-                while (N < ORD && JPRIME) {
-                    while (MULT[N]<J)
-                        MULT[N] += P[N] + P[N];
-                    if (MULT[N] == J)
-                        JPRIME=false;
-                    N++;
-                }
-            } while (!JPRIME);
-            K++;
-            P[K]=J;
+                possiblyPrime = primeTruePoss(Primes, canDidate, ORD, N, multiples);
+            } while (!possiblyPrime);
+            PrimeIndex++;
+            Primes[PrimeIndex]=canDidate;
         }
         PAGENUMBER = 1;
         PAGEOFFSET = 1;
-        while (PAGEOFFSET <= rowNum) {
+
+
+        pageResultprint(1000, 50, 4, Primes, PAGENUMBER, PAGEOFFSET);
+
+    }
+
+    private static boolean primeTruePoss(int[] Primes, int canDidate, int ORD, int N, int[] multiples) {
+        boolean possiblyPrime;
+        possiblyPrime= true;
+        possiblyPrime = isPossiblyPrime(Primes, canDidate, possiblyPrime, ORD, N, multiples);
+        return possiblyPrime;
+    }
+
+    private static boolean isPossiblyPrime(int[] Primes, int canDidate, boolean possiblyPrime, int ORD, int N, int[] multiples) {
+        while (N < ORD && possiblyPrime) {
+            getMultiRowCol(Primes, canDidate, N, multiples);
+            possiblyPrime = isPossiblyPrime(canDidate, possiblyPrime, N, multiples);
+            N++;
+        }
+        return possiblyPrime;
+    }
+
+    private static void getMultiRowCol(int[] Primes, int canDidate, int N, int[] multiples) {
+        while (multiples[N]< canDidate)
+            multiples[N] += getLinePageRowCol(Primes[N], Primes[N]);
+    }
+
+    private static boolean isPossiblyPrime(int canDidate, boolean possiblyPrime, int N, int[] multiples) {
+        if (multiples[N] == canDidate)
+            possiblyPrime =false;
+        return possiblyPrime;
+    }
+
+    private static void pageResultprint(int numberOfPrimes, int linesPerPage, int columnNumbers, int[] Primes, int PAGENUMBER, int PAGEOFFSET) {
+        while (PAGEOFFSET <= numberOfPrimes) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(rowNum));
+            System.out.print(Integer.toString(numberOfPrimes));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(PAGENUMBER));
             System.out.println("\n");
-            for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+colNum-1; ROWOFFSET++) {
-                for (C = 0; C <= Num - 1; C++)
-                    if (ROWOFFSET + C * colNum <= rowNum)
-                        System.out.printf("%10d", P[ROWOFFSET + C * colNum]);
-                System.out.println();
-            }
+
+            pagePrimeprint(numberOfPrimes, linesPerPage, columnNumbers, Primes, PAGEOFFSET);
+
             System.out.println("\f");
             PAGENUMBER++;
-            PAGEOFFSET += colNum*Num;
+            PAGEOFFSET += getColumn(columnNumbers, linesPerPage);
 
         }
+    }
+
+    private static void pagePrimeprint(int numberOfPrimes, int linesPerPage, int columnNumbers, int[] Primes, int PAGEOFFSET) {
+        int column;
+        int ROWOFFSET;
+        for (ROWOFFSET= PAGEOFFSET; ROWOFFSET <= getLinePageRowCol(PAGEOFFSET, linesPerPage) -1; ROWOFFSET++) {
+            for (column = 0; column <= columnNumbers - 1; column++)
+                if (getLinePageRowCol(ROWOFFSET, getColumn(linesPerPage, column)) <= numberOfPrimes)
+                    System.out.printf("%10d", Primes[getLinePageRowCol(ROWOFFSET, getColumn(linesPerPage, column))]);
+            System.out.println();
+        }
+    }
+
+    private static int getColumn(int linesPerPage, int column) {
+        return column * linesPerPage;
+    }
+
+    private static int getLinePageRowCol(int ROWOFFSET, int column) {
+        return ROWOFFSET + column;
     }
 }
